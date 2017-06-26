3e14a619f28dc728827d1bcf310f0e1d47e96759
YakovShechter <ycsycs1@gmail.com>
2017-06-26 22:45:14 +0300

    Logout

--- app/src/main/java/show/example/com/showapplication/MyReceiver.java
+++ app/src/main/java/show/example/com/showapplication/MyReceiver.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import show.example.com.showapplication.Entities.Business;
+
 /**
  * Created by Arye on 20/06/2017.
  */
@@ -17,7 +19,6 @@ public class MyReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-
         Log.d("my service" , "onReceive@@@@@@@@@@@@@");
 
         Toast.makeText(context,intent.getAction(),Toast.LENGTH_LONG).show();
