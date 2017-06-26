25aceb985e63b731bb5f4f54a11caf10d46520ab
arieo <aacc1631@gmail.com>
2017-06-26 22:27:24 +0300

    +entities

--- app/src/main/java/show/example/com/showapplication/MyReceiver.java
+++ app/src/main/java/show/example/com/showapplication/MyReceiver.java
@@ -1,10 +1,14 @@
 package show.example.com.showapplication;
 
+import android.support.v4.app.Fragment;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.widget.Toast;
+
+import static android.app.PendingIntent.getActivity;
 
 /**
  * Created by Arye on 20/06/2017.
@@ -19,7 +23,7 @@ public class MyReceiver extends BroadcastReceiver {
         // an Intent broadcast.
 
         Log.d("my service" , "onReceive@@@@@@@@@@@@@");
-
+       
         Toast.makeText(context,intent.getAction(),Toast.LENGTH_LONG).show();
 
     }
